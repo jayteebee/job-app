5b50696d1790101a9922cc3d382bdfb74966763c
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 14:42:35 +0000

    Add imports and constants to Welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import {icons, SIZES} from "../../../constants"
 
 const Welcome = () => {
   return (
