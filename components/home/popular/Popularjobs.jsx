cb5a194198181ceed0bd38d307d51ebe6d76d520
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 15:44:39 +0000

    Add "Show All" button to Popular Jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,6 +11,9 @@ const Popularjobs = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
