edabb68c09df733b382789637e96925208550894
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 17:15:16 +0000

    Refactor useFetch hook to accept query parameter and handle error

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,7 +4,7 @@ import {RAPID_API_KEY} from "@env"
 
 const rapidApiKey = RAPID_API_KEY
 
-const useFetch = (endpoint) => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(null)
@@ -16,10 +16,30 @@ const useFetch = (endpoint) => {
           'X-RapidAPI-Key': rapidApiKey,
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
-        params: {
-          query: 'Python developer in Texas, USA',
-          page: '1',
-          num_pages: '1'
-        },
+        params: {...query},
       };
+
+      const fetchData = async () => {
+        setIsLoading(true)
+
+        try {
+            const res = await axios.request(options)
+            setData(res.data.data)
+            setIsLoading(false)
+        } catch (error) {
+            setError(error)
+            alert("There's an error")
+        } finally {
+            setIsLoading(false)
+        }
+      }
+
+      useEffect(() => {
+        fetchData()
+      }, [])
+      
+      const refetch = () => {
+        setIsLoading(true)
+        fetchData()
+      }
 }
