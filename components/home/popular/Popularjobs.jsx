e3af05ee3b6bd415308c784ad24d34b72d30bca9
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 15:48:03 +0000

    Add loading and error handling to Popularjobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -7,6 +7,9 @@ import PopularJobCard from "../../common/cards/popular/PopularJobCard"
 
 const Popularjobs = () => {
   const router = useRouter()
+  const isLoading = false
+  const error = false
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -14,6 +17,18 @@ const Popularjobs = () => {
         <TouchableOpacity>
           <Text style={styles.headerBtn}>Show All</Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary}/>
+          ) : error ? (
+            <Text>Something went wrong...</Text>
+          ) : (
+            <FlatList
+            
+            />
+          )}
       </View>
     </View>
   )
