6a57b5f675347a6cafe8ad43f596a5fbbf8e1a7b
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 17:53:38 +0000

    Add checkImageURL import

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 
 import styles from './popularjobcard.style'
+import { checkImageURL } from '../../../../utils'
 
 const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
   console.log('item',item)
