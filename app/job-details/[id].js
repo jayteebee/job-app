a5dc675dcdd768027124ffa0c6106e1c19047bde
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 18:52:45 +0000

    Update job details component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLOR, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
-  return (
-    <div>[id]</div>
-  )
-}
+  return <Text>Job Details</Text>;
+};
 
-export default JobDetails
+export default JobDetails;
