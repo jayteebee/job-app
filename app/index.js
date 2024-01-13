5cff13fcac8ee809983add3cc5cd10d7d7a45f4c
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 12:37:19 +0000

    Add Welcome, Popularjobs, and Nearbyjobs components

--- app/index.js
+++ app/index.js
@@ -31,7 +31,11 @@ const Home = () => {
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={{ flex: 1, padding: SIZES.medium }}>
-          <Welcome />
+          <Welcome
+          
+          />
+          <Popularjobs />
+          <Nearbyjobs />
         </View>
       </ScrollView>
     </SafeAreaView>
