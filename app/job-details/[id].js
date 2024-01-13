2e278531ec5f856badc595010638f65b7785a1b3
Jethro_Block <jethroblock22@gmail.COM>
2024-01-13 19:02:33 +0000

    Fix import and update JobDetails component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -17,7 +17,7 @@ import {
   ScreenHeaderBtn,
   Specifics,
 } from "../../components";
-import { COLOR, icons, SIZES } from "../../constants";
+import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
@@ -28,7 +28,37 @@ const {data, isLoading, error, refetch} = useFetch("job-details", {
     job_id: params.id
 })
 
-  return <Text>Job Details</Text>;
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+        <Stack.Screen
+        options={{
+            headerStyle: {backgroundColor: COLORS.lightWhite},
+            headerShadowVisible: false,
+            headerBackVisible: false,
+            headerLeft: () => {
+                <ScreenHeaderBtn 
+                iconUrl={icons.left}
+                dimension="60%"
+                handlePress={() => router.back()}
+                />
+            },
+            headerRight: () => {
+                <ScreenHeaderBtn 
+                iconUrl={icons.share}
+                dimension="60%"
+                />
+            },
+            headerTitle: ""
+        }}
+        />
+        <>
+            <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+            
+            </ScrollView>
+        </>
+
+    </SafeAreaView>
+  )
 };
 
 export default JobDetails;
