4a0ef4449cd66bbef16e32ccf1d006afa664adae
Jethro_Block <jethroblock22@gmail.COM>
2024-01-14 10:45:39 +0000

    Add tabs functionality to JobDetails component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,10 +20,13 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
+const tabs = ["About", "Qualifications", "Responsibilities"]
+
 const JobDetails = () => {
   const params = useGlobalSearchParams();
   const router = useRouter();
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0])
 
   const onRefresh = () => {};
 
@@ -73,7 +76,11 @@ const JobDetails = () => {
               location={data[0].job_country}
               />
 
-              <JobTabs />
+              <JobTabs
+              tabs={tabs}
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+              />
             </View>
           )}
         </ScrollView>
