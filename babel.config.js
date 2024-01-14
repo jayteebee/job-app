19443c674d99a8e8fb2d596d05936ccc589464a1
Jethro_Block <jethroblock22@gmail.COM>
2024-01-14 11:54:08 +0000

    Refactor babel.config.js presets

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "@babel/preset-env"],
+    presets: ["babel-preset-expo"],
     plugins: [
       "expo-router/babel",
-      "@babel/plugin-proposal-optional-chaining",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
     ],
   };
 };
