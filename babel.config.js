bd36cb2ec7c1a7f0412bf63e44535c3302438789
Jethro_Block <jethroblock22@gmail.COM>
2024-01-14 11:07:13 +0000

    Add Babel plugins for optional chaining and nullish coalescing operator

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
+    plugins: ["expo-router/babel",    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-nullish-coalescing-operator"],
+    
   };
 };
