50751718a39c4750453f7647564707371391782c
Jethro_Block <jethroblock22@gmail.COM>
2024-01-14 11:25:43 +0000

    Update Babel dependencies and presets

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel",    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator"],
-    
+    presets: ["babel-preset-expo", "@babel/preset-env"],
+    plugins: [
+      "expo-router/babel",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+    ],
   };
 };
