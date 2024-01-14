41742d46a65fae11c82e0771339894fd4f9d397d
Jethro_Block <jethroblock22@gmail.COM>
2024-01-14 12:26:43 +0000

    Add TouchableOpacity, Image, Linking and icons import to Footer component

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Linking } from 'react-native'
 
 import styles from './footer.style'
+import { icons } from '../../../constants'
+
 
 const Footer = () => {
   return (
